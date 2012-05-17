dc919a140b819fddc85ce64dc0aac89e174440ae
sandra1n <newqwers@gmail.com>
2012-05-17 13:32:22 +0300

    remove getdb

--- iwi/Iwi.php
+++ iwi/Iwi.php
@@ -116,8 +116,8 @@ class Iwi extends Image
 
     public function verifyTable()
     {
-        if (!Yii::app()->getDb()->schema->getTable('storage')) {
-            Yii::app()->getDb()->createCommand()->createTable("storage", array(
+        if (!Yii::app()->db->schema->getTable('storage')) {
+            Yii::app()->db->createCommand()->createTable("storage", array(
                 'key' => 'string',
                 'value' => 'text',
             ));
